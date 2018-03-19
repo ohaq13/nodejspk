f9f00de2f639e85c3c5bc5fc5ff763a690c36acd
Owais <ohaq@us.ibm.com>
2018-03-19 16:28:28 -0400

    learning

--- app/common/js/customer.js
+++ app/common/js/customer.js
@@ -25,12 +25,13 @@ define(['db'], function (db) {
  
         getCustomerByName1 (req, res){
             let param = "%" +req.query.q + "%";                  
-            db.select(`SELECT id ${FIELDS.id}, name ${FIELDS.name} FROM customer where name like ?`, [param], done); 
+            db.select(`SELECT id ${FIELDS.id}, name ${FIELDS.name}, name text FROM customer where name like ?`, [param], done); 
 
             function done(err, data, count){
                 if (err) {
                     return res.send(err.message);
                 }
+                
                 res.json(data);
             }
         }
